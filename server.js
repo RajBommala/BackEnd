c8303619edfd7041b8f148c9269ef54791439b9f
Bommala <basusmy@gmail.com>
2018-10-13 19:55:58 -0500

    updated

--- server.js
+++ server.js
@@ -19,18 +19,16 @@ const db = knex({
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> { res.send('it is working!') })
+app.get('/', (req, res)=> { res.send('it is working!'); })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`app is running on port ${PORT}`);
-})
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
+});
