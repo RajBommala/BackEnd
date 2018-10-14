7a0db8dab1644c36434135f3f2544d0fb0597be4
Bommala <basusmy@gmail.com>
2018-10-14 02:17:09 -0500

    updated

--- controllers/image.js
+++ controllers/image.js
@@ -1,31 +1,29 @@
 const Clarifai = require('clarifai');
 
-//You must add your own API key here from Clarifai.
-const app = new Clarifai.App({
- apiKey: process.env.API_CLARIFAI
-});
+// Clarifai API configuration
+const app = new Clarifai.App({apiKey: process.env.API_CLARIFAI});
 
-const handleApiCall = (req, res) => {
+const handleAPICall = (req, res) => {
   app.models
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
       res.json(data);
     })
-    .catch(err => res.status(400).json('unable to work with API'))
+    .catch(err => res.status(400).json('unable to work with API'));
 }
 
 const handleImage = (req, res, db) => {
-  const { id } = req.body;
+  const {id} = req.body;
   db('users').where('id', '=', id)
   .increment('entries', 1)
   .returning('entries')
   .then(entries => {
     res.json(entries[0]);
   })
-  .catch(err => res.status(400).json('unable to get entries'))
+  .catch(err => res.json('Unable to get entries'));
 }
 
 module.exports = {
-  handleImage,
-  handleApiCall
+  handleImage: handleImage,
+  handleAPICall: handleAPICall
 }
--- controllers/profile.js
+++ controllers/profile.js
@@ -1,16 +1,17 @@
+
 const handleProfileGet = (req, res, db) => {
-  const { id } = req.params;
+  const {id} = req.params;
   db.select('*').from('users').where({id})
-    .then(user => {
-      if (user.length) {
-        res.json(user[0])
-      } else {
-        res.status(400).json('Not found')
-      }
-    })
-    .catch(err => res.status(400).json('error getting user'))
+  .then( user => {
+    if (user.length) {
+      res.json(user[0]);
+    } else {
+      res.status(400).json('User not found!');
+    }
+  })
+  .catch(err => res.status(400).json('Error getting user'));
 }
 
 module.exports = {
-  handleProfileGet
+  handleProfileGet: handleProfileGet
 }
