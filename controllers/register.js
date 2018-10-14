ffadc5d628c0a43fe011612d6a7c22f46745a073
Bommala <basusmy@gmail.com>
2018-10-14 15:06:15 -0500

    updated

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,5 @@
 const handleRegister = (req, res, db, bcrypt) => {
-  const { email, name, password } = req.body;
+  const { name, email, password } = req.body;
   if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');
   }
@@ -15,8 +15,8 @@ const handleRegister = (req, res, db, bcrypt) => {
         return trx('users')
           .returning('*')
           .insert({
-            email: loginEmail[0],
             name: name,
+            email: loginEmail[0],
             joined: new Date()
           })
           .then(user => {
@@ -26,7 +26,7 @@ const handleRegister = (req, res, db, bcrypt) => {
       .then(trx.commit)
       .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json('unable to register'))
+    .catch(err => res.status(400).json('unable to register'));
 }
 
 module.exports = {
--- controllers/signin.js
+++ controllers/signin.js
@@ -1,4 +1,4 @@
-const handleSignin = (db, bcrypt) => (req, res) => {
+const handleSignin = (req, res, bcrypt, db) => {
   const { email, password } = req.body;
   if (!email || !password) {
     return res.status(400).json('incorrect form submission');
@@ -13,12 +13,12 @@ const handleSignin = (db, bcrypt) => (req, res) => {
           .then(user => {
             res.json(user[0])
           })
-          .catch(err => res.status(400).json('unable to get user'))
+          .catch(err => res.status(400).json('unable to get user'));
       } else {
-        res.status(400).json('wrong credentials')
+        res.status(400).json('wrong credentials');
       }
     })
-    .catch(err => res.status(400).json('wrong credentials'))
+    .catch(err => res.status(400).json('wrong credentials'));
 }
 
 module.exports = {
